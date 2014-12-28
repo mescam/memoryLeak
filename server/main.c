9766b13a06151b9021247a0a0ff9e56aac0fc560
Jakub Woźniak <mescam@mescam.pl>
2014-12-28 20:27:00 +0100

    creating epoll fds, creating socket, listening for connections, selecting workers

--- server/main.c
+++ server/main.c
@@ -21,82 +21,186 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+#define MAX_EVENTS  10
+#define QUEUE_SIZE  5
 
 struct _settings 
 {
     char *interface; // address of interface
-    uint port; // listening port
-    uint memsize; // size of memory cache in bytes
+    ushort port; // listening port
+    unsigned long long int memsize; // size of memory cache in bytes
     char *location; // where are our files
+    uint workers_count; // how many workers should we spawn
 };
 
-int
-main(int argc, char **argv)
-{
-    struct _settings settings;
-    char c;
-    settings.port = settings.memsize = 0;
-    settings.interface = settings.location = (char*) 0;
+int running = 1;
 
-    int param_count = 0;
-    
-    // handle application parameters
-    while ((c = getopt(argc, argv, "i:p:s:w:hv")) != -1)
-    {
-        switch (c)
-        {
-            case 'i': // interface
-                settings.interface = malloc(strlen(optarg) * sizeof(char));
-                strcpy(settings.interface, optarg);
-                param_count++;
+void
+print_version()
+{
+    printf("memoryLeak, version 1.0\n"
+           "Simple memcache server for files\n\n"
+           "Copyright (C) 2014\t"
+           "Jakub Woźniak <jakub at wozniak dot im>\n"
+           "Project for Computer Networks course\n"
+           "Faculty of Computing, Poznan University of Technology\n\n"
+           "License GPLv3+: GNU GPL version 3 or later "
+           "<http://gnu.org/licenses/gpl.html>\nThis is free software;"
+           " you are free to change and redistribute it.\nThere is "
+           "NO WARRANTY, to the extent permitted by law.\n");
+}
+
+void
+handle_arguments(int argc, char **argv, struct _settings *settings,
+                 int *param_count)
+{
+    char c;
+    while ((c = getopt(argc, argv, "I:P:s:w:c:hv")) != -1) {
+        switch (c) {
+            case 'I': // interface
+                settings->interface = malloc(strlen(optarg) * sizeof(char));
+                strcpy(settings->interface, optarg);
+                (*param_count)++;
                 break;
 
-            case 'p': // port
-                settings.port = atoi(optarg);
-                param_count++;
+            case 'P': // port
+                settings->port = atoi(optarg);
+                (*param_count)++;
                 break;
 
             case 's': // size of memory cache
-                settings.memsize = atoi(optarg);
-                param_count++;
+                settings->memsize = atoi(optarg);
+                (*param_count)++;
                 break;
 
             case 'w': // directory with files to serve
-                settings.location = malloc(strlen(optarg) * sizeof(char));
-                strcpy(settings.location, optarg);
-                param_count++;
+                settings->location = malloc(strlen(optarg) * sizeof(char));
+                strcpy(settings->location, optarg);
+                (*param_count)++;
+                break;
+
+            case 'c': // workers count
+                settings->workers_count = atoi(optarg);
                 break;
 
             case 'v': // version
-                // TODO: should be somewhere else? :X 
-                printf("memoryLeak, version 1.0\n"
-                "Simple memcache server for files\n\n"
-                "Copyright (C) 2014\t"
-                "Jakub Woźniak <jakub at wozniak dot im>\n"
-                "Project for Computer Networks course\n"
-                "Faculty of Computing, Poznan University of Technology\n\n"
-                "License GPLv3+: GNU GPL version 3 or later "
-                "<http://gnu.org/licenses/gpl.html>\nThis is free software;"
-                " you are free to change and redistribute it.\nThere is "
-                "NO WARRANTY, to the extent permitted by law.\n");
-                param_count = -100;
+                print_version();
+                (*param_count) = -100;
                 break;
 
             default:
                 printf("[!] Unrecognized argument %c!\n", c);
         }
     }
+}
 
-    if (param_count == 4) // all arguments specified, run application
-    {
+int
+create_socket(struct _settings *settings, int *socket_fd)
+{
+    struct sockaddr_in st_addr;
+    int n_opt = 1;
+
+    // fill sockaddr struct with data from settings
+    memset(&st_addr, 0, sizeof(struct sockaddr));
+    st_addr.sin_family = AF_INET;
+    st_addr.sin_addr.s_addr = inet_addr(settings->interface);
+    st_addr.sin_port = htons(settings->port);
+
+    // create socket!
+    *socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*socket_fd < 0) {
+        perror("[!] Error during creating socket");
+        return 1;
+    }
+    setsockopt(*socket_fd, SOL_SOCKET, SO_REUSEADDR,
+              (char*)&n_opt, sizeof(n_opt));
+
+    if(bind(*socket_fd, (struct sockaddr*)&st_addr, sizeof(struct sockaddr))) {
+        perror("[!] Binding error");
+        return 1;
+    }
+
+    if(listen(*socket_fd, QUEUE_SIZE)) {
+        perror("[!] listen() error");
+        return 1;
+    }
+
+    return 0;
+}
+
+void
+run(struct _settings *settings)
+{
+    // array for epoll filedescriptors for every worker
+    int *workers_epoll = malloc(sizeof(int) * settings->workers_count);
+    int i;
+    int rr_worker = 0; // round robin worker selector
+    int socket_fd;
+
+    // create epoll for every worker
+    for(i = 0; i <= settings->workers_count; ++i) {
+        workers_epoll[i] = epoll_create(MAX_EVENTS);
+    }
+    printf("[+] Spawned worker threads\n");
+
+    if(!create_socket(settings, &socket_fd)) {
+        // accept!
+        int client_fd;
+        struct sockaddr_in client_addr;
+        socklen_t n_tmp;
+        n_tmp = sizeof(struct sockaddr);
+
+        while (running) {
+            client_fd = accept(socket_fd, (struct sockaddr*) &client_addr,
+                               &n_tmp);
+            if (client_fd < 0) {
+                printf("[!] Error when accepting incoming connection\n");
+            }
+            printf("[+] Incoming connection for worker %d\n", rr_worker+1);
+
+            rr_worker = (rr_worker + 1) % settings->workers_count;
+        }
+
+    } else {
+        printf("[!] Aborting\n");
+    }
+
+    // close worker's epolls
+    for(i = 0; i <= settings->workers_count; ++i)
+        close(workers_epoll[i]);
+
+    free(workers_epoll);
+    if (socket_fd > 0)
+        close(socket_fd);
+}
+
+int
+main(int argc, char **argv)
+{
+    struct _settings settings;
+    settings.port = settings.memsize = 0;
+    settings.interface = settings.location = (char*) 0;
+    settings.workers_count = sysconf(_SC_NPROCESSORS_ONLN);
+    int param_count = 0;
+    
+    // handle application parameters
+    handle_arguments(argc, argv, &settings, &param_count);
+    
+    if (param_count == 4) { // all arguments specified, run application
         printf("[+] memoryLeak starting up!\n");
         printf("[+] Files directory: %s\n", settings.location);
-        printf("[ ] Configured to listen on %s:%d with memory size "
-               "of %d bytes\n",
-               settings.interface, settings.port, settings.memsize);
-    } 
-    else if (param_count > 0) // show warning
-    {
+        printf("[+] Configured to listen on %s:%d with memory size "
+               "of %lld bytes and %d workers\n",
+               settings.interface, settings.port, settings.memsize,
+               settings.workers_count);
+        run(&settings);
+    } else if (param_count > 0) { // show warning
         printf("[!] Missing arguments\n");
     }
 
